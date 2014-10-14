b65fe53f8d6abeadde0dab7568a69d9f8fca8c5b
xD4rk97 <luigidalex97@gmail.com>
2014-10-14 21:36:36 +0200

    Punto.java

---
+++ Punto.java
@@ -0,0 +1,28 @@
+/**
+ *
+ * @author xD4rk97
+ */
+public class Punto {
+float x;
+float y;
+Punto (float x, float y) {
+    this.x=x;
+    this.y=y;
+}
+Punto (){
+    x=0;
+    y=0;
+}
+public void setX (float x){
+    this.x=x;
+}
+public void setY(float y){
+    this.y=y;
+}
+public float getX(){
+    return x;
+}
+public float getY(){
+return y;    
+  }
+}
